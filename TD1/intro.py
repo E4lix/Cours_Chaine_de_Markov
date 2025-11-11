afb198cbdfa3b817f03c720a4f5361e1cc98ab19
E4lix <ealix95@gmail.com>
2025-11-11 23:28:38 +0100

    Modification Code

--- TD1/intro.py
+++ TD1/intro.py
@@ -29,8 +29,6 @@ waits = [r.waiting_time for r in recs if r.node==1]
 print(sum(waits)/len(waits))
 
 
-
-
 net_c = ciw.create_network(
     arrival_distributions=[ciw.dists.Gamma(shape=2.0, scale=15.0)],
     service_distributions=[ciw.dists.Uniform(15, 35)],
